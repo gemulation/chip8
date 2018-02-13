a8b515823ea355d15c01b243ba11ee724c9fd7ce
Jairo Luiz <t4ngz3r0@gmail.com>
2018-02-13 11:31:54 -0300

    rename repo

--- rom_test.go
+++ rom_test.go
@@ -3,12 +3,12 @@ package chip8_test
 import (
 	"testing"
 
-	"github.com/gemu/chip8"
+	"github.com/gemulation/chip8"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewRom(t *testing.T) {
-	rom, err := chip8.NewROM("../roms/pong.rom")
+	rom, err := chip8.NewROM("roms/pong.rom")
 
 	require.Nil(t, err)
 	require.NotNil(t, rom)
